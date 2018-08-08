876423829efcb09569ae61f6e157c3c875cda126
encry1024 <register.chihiro@gmail.com>
2018-08-08 23:35:38 +0900

    Update init.el by Custom

--- init.el
+++ init.el
@@ -20,3 +20,15 @@
 (setq straight-use-package-by-default t)
 (use-package init-loader)
 (init-loader-load "~/.emacs.d/inits")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
