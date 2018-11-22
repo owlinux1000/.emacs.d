77becd3885d297274fdd26d95d309daf54a6599a
chihiro-h <chihiro-h@iij.ad.jp>
2018-11-22 23:09:45 +0900

    TypeScriptのインデントを２つに設定

--- inits/25_typescript.el
+++ inits/25_typescript.el
@@ -1,5 +1,10 @@
 (use-package typescript-mode
 
   :mode "\\.ts"
-  
-  )
+  :init
+  (add-hook 'typescript-mode-hook
+            (lambda() 
+              (setq indent-tabs-mode nil)
+              (setq typescript-indent-level 2)
+              ))
+)
