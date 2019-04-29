1cb6dbfbc5014a8bcc996fc8652c4e0419ad4fbb
encry1024 <encry1024@gmail.com>
2019-04-30 07:57:43 +0900

    Update typescript config

--- inits/25_typescript.el
+++ inits/25_typescript.el
@@ -1,5 +1,10 @@
 (use-package typescript-mode
 
   :mode "\\.ts"
-  
+  :init
+  (add-hook 'typescript-mode-hook
+            (lambda()
+              (setq typescript-indent-level 2)
+              )
+            )
   )
