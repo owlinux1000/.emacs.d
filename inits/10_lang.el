5b133c89b2116ea380ea5cec78bd38d81ef7f2f9
owlinux1000 <encry1024@gmail.com>
2023-06-30 20:21:01 +0900

    (update): Add several language modes

--- inits/10_lang.el
+++ inits/10_lang.el
@@ -9,3 +9,23 @@
 (use-package rust-mode
   :mode "\\.rs$"
   )
+
+(use-package go-mode
+  :mode "\\.go$"
+  )
+
+(use-package yaml-mode
+  :mode "\\.ya?ml$"
+  )
+
+(use-package json-mode
+  :mode "\\.json$"
+  )
+
+(use-package toml-mode
+  :mode "\\.toml$"
+  )
+
+(use-package dockerfile-mode
+  :mode "Dockerfile"
+  )
